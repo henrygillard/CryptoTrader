87c33fd6601408d81a009d02ddda02eb070984ee
Henry Gillard <henry@MacBook-Pro.local>
2021-11-10 16:22:51 -0800

    initial commit

---
+++ cryptotradesapp/views.py
@@ -0,0 +1,13 @@
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return HttpResponse("<h1>About Page</h1>")
+
+
+
+# Create your views here.
